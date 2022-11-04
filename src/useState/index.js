47b994715292af7177872b35b18a71c6f15d122a
wp-dukel <duke.l@webprovise.com>
2022-11-04 17:40:24 +0700

    init

---
+++ src/useState/index.js
@@ -0,0 +1,18 @@
+const useStateDemo = () => {
+    const [count, setCount] = useState(1);
+
+    function handleIncrease() {
+        setCount(count + 1);
+        setCount(count + 1);
+        setCount(prevState => prevState + 1);
+    }
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+        </>
+    )
+}
+
+export default useStateDemo;
